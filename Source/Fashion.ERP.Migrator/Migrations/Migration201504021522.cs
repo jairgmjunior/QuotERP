a2e3e12c3521953339e365088b6423b325d707e4
Victor <victor@fashionsistemas.com.br>
2015-04-06 09:05:25 -0300

    Relatório Materiais de Modelos Aprovados. Permissões.

---
+++ Source/Fashion.ERP.Migrator/Migrations/Migration201504021522.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+
+namespace Fashion.ERP.Migrator
+{
+    [Migration(201504021522)]
+    public class Migration201504021522 : Migration
+    {
+        public override void Up()
+        {
+            Execute.EmbeddedScript("Fashion.ERP.Migrator.Scripts._201504021522.permissao.sql");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Source/Fashion.ERP.Migrator/Scripts/201504021522/permissao.sql
@@ -0,0 +1,5 @@
+﻿DECLARE @RELATORIOID AS BIGINT, @ID AS BIGINT;
+SET @RELATORIOID = (SELECT id FROM PERMISsao where descricao = 'Relatórios' and area ='EngenhariaProduto');
+
+INSERT INTO permissao (Action, Area, Controller, Descricao, ExibeNoMenu, RequerPermissao, Ordem, permissaopai_id) 
+VALUES ('MateriaisModelosAprovados', 'EngenhariaProduto', 'Relatorio', 'Materiais de Modelos Aprovados',1 ,1,0, @RELATORIOID);
