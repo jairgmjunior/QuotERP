2bdbb96b38332547df91cd083a484fb9ec41817b
Victor <victor@fashionsistemas.com.br>
2015-04-22 10:16:15 -0300

    Modelo de domínio da avaliação do Modelo.

---
+++ Source/Fashion.ERP.Mapping/EngenhariaProduto/ModeloAprovacaoMap.cs
@@ -0,0 +1,23 @@
+﻿using Fashion.ERP.Domain.EngenhariaProduto;
+using Fashion.Framework.Mapping;
+
+namespace Fashion.ERP.Mapping.EngenhariaProduto
+{
+    public class ModeloAprovacaoMap: EmpresaClassMap<ModeloAprovacao>
+    {
+        public ModeloAprovacaoMap()
+            : base("modeloaprovacao", 0)
+        {
+            Map(x => x.Quantidade).Not.Nullable();
+            Map(x => x.Observacao).Length(250);
+            Map(x => x.Referencia).Not.Nullable();
+            Map(x => x.Descricao).Not.Nullable();
+            Map(x => x.MedidaBarra);
+
+            References(x => x.Comprimento);
+            References(x => x.Barra);
+            References(x => x.ProdutoBase);
+            References(x => x.FichaTecnica);
+        }
+    }
+}
