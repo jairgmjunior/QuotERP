008768846d744f516da81322f68527692127a0e2
Victor <victor@fashionsistemas.com.br>
2015-05-27 17:43:47 -0300

    Alteração do tipo da coluna quantidade na tabela reservaestoquematerial para float.

---
+++ Source/Fashion.ERP.Migrator/Migrations/Migration201505271726.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+
+namespace Fashion.ERP.Migrator
+{
+    [Migration(201505271726)]
+    public class Migration201505271726 : Migration
+    {
+        public override void Up()
+        {
+            Execute.EmbeddedScript("Fashion.ERP.Migrator.Scripts._201505271726.reservaestoquematerial.sql");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
